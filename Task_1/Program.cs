be389e2c70acf57ddc129a615bb2584f28641007
liza <lizashatuzhilkina@gmail.com>
2022-04-25 22:56:20 +0300

    добавила условия

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число 1 -> ");
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите число 1 -> ");
 int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число 2 -> ");
 int num2 = int.Parse(Console.ReadLine());
