3cdac42a191d8e0e77e19d772b835795f77f3a26
liza <lizashatuzhilkina@gmail.com>
2022-04-25 21:21:17 +0300

    task01

---
+++ Task_0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите целое число -> ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write("Квадрат числа равен -> ");
+Console.WriteLine(numResult);
+Console.ReadKey();
+
---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число -> ");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число -> ");
+int numB = int.Parse(Console.ReadLine());
+if(numB * numB == numA )
+{
+    Console.WriteLine("true");
+}
+else 
+{
+    Console.WriteLine("folse");
+}
